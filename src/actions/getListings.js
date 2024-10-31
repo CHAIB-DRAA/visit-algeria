32e3e906bfdd36509a592d0929dc0abb271da2dd
CHAIB-DRAA <108793172+CHAIB-DRAA@users.noreply.github.com>
2024-10-31 13:23:39 +0100

    Update getListings.js

--- src/actions/getListings.js
+++ src/actions/getListings.js
@@ -24,25 +24,25 @@ export default async function getListings(params) {
 		}
 
 		const skip = (parsedPage - 1) * parsedPageSize;
+
+		// Construction de la requête pour le comptage
 		const totalListings = await prisma.listing.count({
 			where: {
 				AND: [
-					{ title: { contains: title } },
-					{
-						OR: [query],
-					},
+					{ title: { contains: title || "", mode: "insensitive" } }, // Ajout de mode insensible à la casse
+					query, // Utilisation directe de query
 				],
 			},
 		});
+
 		const totalPages = Math.ceil(totalListings / parsedPageSize);
 
+		// Récupération des annonces
 		const listings = await prisma.listing.findMany({
 			where: {
 				AND: [
-					{ title: { contains: title } },
-					{
-						OR: [query],
-					},
+					{ title: { contains: title || "", mode: "insensitive" } }, // Ajout de mode insensible à la casse
+					query, // Utilisation directe de query
 				],
 			},
 			skip: skip,
@@ -70,6 +70,7 @@ export default async function getListings(params) {
 			totalListings,
 		};
 	} catch (error) {
-		throw new Error(error);
+		console.error("Error fetching listings:", error); // Ajout d'un log d'erreur
+		throw new Error("Could not fetch listings. Please try again later.");
 	}
 }
