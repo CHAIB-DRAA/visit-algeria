0d99a20a865901d0be31a2c79dd373eba7c71148
CHAIB-DRAA <108793172+CHAIB-DRAA@users.noreply.github.com>
2024-10-31 13:35:39 +0100

    Update route.js

--- src/app/api/listings/review/[listingId]/route.js
+++ src/app/api/listings/review/[listingId]/route.js
@@ -1,30 +1,44 @@
 import { NextResponse } from "next/server";
-import { getCurrentUser } from "@/actions/getCurrentUser";
+import { getCurrentUser  } from "@/actions/getCurrentUser";
 import prisma from "@/libs/prismadb";
 
 export async function POST(request, { params }) {
-	const currentUser = await getCurrentUser();
-	if (!currentUser) {
+	const currentUser  = await getCurrentUser ();
+	if (!currentUser ) {
 		return NextResponse.error();
 	}
+
 	const { listingId } = params;
 
 	if (!listingId) {
-		throw new Error("Invalid ID");
+		return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
 	}
 
 	const body = await request.json();
-
 	const { rating, comment } = body;
 
-	const review = await prisma.review.create({
-		data: {
-			userId: currentUser.id,
-			listingId: parseInt(listingId),
-			rating: parseInt(rating.value),
-			comment: comment,
-		},
-	});
+	if (!rating || !comment) {
+		return NextResponse.json({ error: "Invalid request body" }, { status: 400 });
+	}
 
-	return NextResponse.json(review);
+	const ratingValue = parseInt(rating.value);
+	if (isNaN(ratingValue)) {
+		return NextResponse.json({ error: "Invalid rating" }, { status: 400 });
+	}
+
+	try {
+		const review = await prisma.review.create({
+			data: {
+				userId: currentUser .id,
+				listingId: listingId, // Pass listingId as a string
+				rating: ratingValue,
+				comment: comment,
+			},
+		});
+
+		return NextResponse.json(review);
+	} catch (error) {
+		console.error("Error creating review:", error);
+		return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+	}
 }
