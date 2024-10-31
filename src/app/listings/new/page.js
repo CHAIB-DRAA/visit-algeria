2e98eea06ee0428570dbd8180d355abbecdf0c54
CHAIB-DRAA <108793172+CHAIB-DRAA@users.noreply.github.com>
2024-10-31 13:09:20 +0100

    Update page.js

--- src/app/listings/new/page.js
+++ src/app/listings/new/page.js
@@ -1,40 +1,22 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
 import PageBanner from "@/components/Common/PageBanner";
 import ListingForm from "@/components/Listings/New/ListingForm";
-import { getCurrentUser } from "@/actions/getCurrentUser";
+import {getCurrentUser} from "@/actions/getCurrentUser";
+import {redirect} from "next/navigation";
 
-export const dynamic = "force-dynamic";
-
-const Page = () => {
-	const router = useRouter();
-	const [isAdmin, setIsAdmin] = useState(null);
-
-	useEffect(() => {
-		const fetchUser = async () => {
-			const currentUser = await getCurrentUser();
-			const userIsAdmin = currentUser?.role === "ADMIN";
-			setIsAdmin(userIsAdmin);
-
-			if (!userIsAdmin) {
-				router.push("/");
-			}
-		};
-
-		fetchUser();
-	}, [router]);
-
-	// Évite l'affichage du contenu jusqu'à la fin de la vérification
-	if (isAdmin === null) return null;
-
+const page = async () => {
+	const currentUser = await getCurrentUser();
+	const isAdmin = currentUser?.role === "ADMIN";
+	if (!isAdmin) {
+		redirect("/");
+	}
 	return (
 		<>
 			<PageBanner pageTitle="Add Listing" />
+
 			<ListingForm />
 		</>
 	);
 };
 
-export default Page;
+export default page;
