f18cd2409320abb23ccbbfec84cdccfe65627489
CHAIB-DRAA <108793172+CHAIB-DRAA@users.noreply.github.com>
2024-11-05 11:05:30 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-'use client'
-import React, { useState, useEffect } from "react";
 import Banner from "@/components/Index/Banner";
 import Blog from "@/components/Index/Blog";
 import Category from "@/components/Index/Category";
@@ -10,48 +8,34 @@ import Partner from "@/components/Index/Partner";
 import Subscribe from "@/components/Index/Subscribe";
 import Testimony from "@/components/Index/Testimony";
 import WorkArea from "@/components/Index/WorkArea";
-import { getCurrentUser  } from "@/actions/getCurrentUser ";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 import getBlogPosts from "@/actions/getBlogPosts";
 
 export const dynamic = "force-dynamic";
 
 const limitParams = { limit: 6 };
 
-export default function Home() {
-	const [currentUser , setCurrentUser ] = useState(null);
-	const [blogPosts, setBlogPosts] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(null);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const user = await getCurrentUser ();
-				const posts = await getBlogPosts(limitParams);
-				setCurrentUser (user);
-				setBlogPosts(posts);
-			} catch (err) {
-				setError(err);
-			} finally {
-				setLoading(false);
-			}
-		};
-
-		fetchData();
-	}, []);
-
-	if (loading) return <div>Loading...</div>;
-	if (error) return <div>Error: {error.message}</div>;
+export default async function Home() {
+	const currentUser = await getCurrentUser();
+	const blogPosts = await getBlogPosts(limitParams);
 
 	return (
 		<>
+
 			<Banner />
+
 			<Category />
-			<Featured currentUser ={currentUser } />
+
+			<Featured currentUser={currentUser} />
+
 			<Locations />
+
 			<WorkArea />
+
 			<Testimony />
+
 			<Favour />
+
 			<Subscribe />
 			<Blog blogPosts={blogPosts} />
 		</>
