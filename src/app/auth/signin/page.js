cdb0e500582efbee48b35338852dded549e0aa27
CHAIB-DRAA <108793172+CHAIB-DRAA@users.noreply.github.com>
2024-11-05 11:06:52 +0100

    Update page.js

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import { redirect } from "next/navigation";
 import PageBanner from "@/components/Common/PageBanner";
 import SigninForm from "@/components/Auth/SigninForm";
-import { getCurrentUser  } from "@/actions/getCurrentUser ";
+import { getCurrentUser  } from "@/actions/getCurrentUser";
 
 const Page = async () => {
 	const currentUser  = await getCurrentUser ();
