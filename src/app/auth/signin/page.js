a15bf8ae9bca5296659c66bda77433581241c7e1
CHAIB-DRAA <108793172+CHAIB-DRAA@users.noreply.github.com>
2024-11-05 11:12:54 +0100

    Update page.js

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -1,31 +1,21 @@
-"use client"
-import React, { useState } from "react";
-import { useRouter } from "next/router";
+import React from "react";
 import { redirect } from "next/navigation";
 import PageBanner from "@/components/Common/PageBanner";
 import SigninForm from "@/components/Auth/SigninForm";
-import { getCurrentUser  } from "@/actions/getCurrentUser";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
-const Page = async () => {
-	const currentUser  = await getCurrentUser ();
-	const router = useRouter();
-	const [isAuthenticated, setIsAuthenticated] = useState(!!currentUser );
-
-	if (isAuthenticated) {
+const page = async () => {
+	const currentUser = await getCurrentUser();
+	if (currentUser) {
 		redirect("/");
 	}
-
-	const handleSignin = () => {
-		setIsAuthenticated(true);
-		router.push("/"); // Redirige vers la page d'accueil après la connexion
-	};
-
 	return (
 		<>
 			<PageBanner pageTitle="Signin" />
-			<SigninForm onSignin={handleSignin} />
+			
+			<SigninForm />
 		</>
 	);
 };
 
-export default Page;
+export default page;
