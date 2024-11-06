90882d5a8a183c47ce4d6e0415e9a778a0dde25e
CHAIB-DRAA <108793172+CHAIB-DRAA@users.noreply.github.com>
2024-11-06 11:56:05 +0100

    Update BlogContent.js

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -2,7 +2,7 @@
 import React from "react";
 import BlogCard from "./BlogCard";
 
-const BlogContent = ({ blogPosts }) => {
+const BlogContent = ({ blogPosts = [] }) => { // Valeur par défaut pour blogPosts
 	return (
 		<div className="blog-area ptb-100">
 			<div className="container">
@@ -11,9 +11,13 @@ const BlogContent = ({ blogPosts }) => {
 				</div>
 
 				<div className="row justify-content-center">
-					{blogPosts.map((blog) => (
-						<BlogCard key={blog.id} {...blog} />
-					))}
+					{blogPosts.length > 0 ? ( // Vérification si blogPosts contient des éléments
+						blogPosts.map((blog) => (
+							<BlogCard key={blog.id} {...blog} />
+						))
+					) : (
+						<p>Aucun article de blog disponible.</p> // Message alternatif si aucun article n'est trouvé
+					)}
 				</div>
 			</div>
 		</div>
